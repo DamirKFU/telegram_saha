f26121c91cf5605db795f49ae489c54aa66e258b
DamirKFU <shabievdg@yandexlyceum.ru>
2024-04-03 13:16:45 +0300

    add: add new app

--- django_telegram/manage.py
+++ django_telegram/manage.py
@@ -4,19 +4,23 @@ import os
 import sys
 
 
+__all__ = []
+
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_telegram.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_telegram.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environment?",
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
